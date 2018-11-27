c07171c5dd79bfb5e4270841308efe7806c2a88a
Tatiana Morales <tmorales17@jaumebalmes.net>
2018-11-27 20:35:00 +0100

    Primer commit

---
+++ app/src/main/java/com/example/tmorales17/myteamapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tmorales17.myteamapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
